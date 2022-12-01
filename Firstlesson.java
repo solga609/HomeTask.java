6fa8490d51a6e7f183a7550cddd087bbaae56099
Olga <olga609@mail.ru>
2022-12-01 13:43:57 +0200

    SecondLesson

--- Firstlesson.java
+++ Firstlesson.java
@@ -1,25 +1,29 @@
-import java.util.Scanner;
+
 
 public class Firstlesson {
-  
-        public class MainClass {
-            public static int PowA(int a, int b)
-                {
-                    if (b==0)
-                        return 1;
-                    return PowA(a,b-1)*a;
+    public static void bubbleSort(int[] sortArr){
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for(int j = 0; j < sortArr.length - i - 1; j++) {
+                if(sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
                 }
-            
-            public static void main(String[] args) 
-            {
-                Scanner myObj = new Scanner(System.in);  
-                System.out.print("a = ");
-                int a = myObj.nextInt();
-                System.out.print("b = ");
-                int b = myObj.nextInt();
-                int pow=PowA(a,b);
-                System.out.println("Pow("+a+", "+b+") = "+pow);
             }
         }
+    }
+    
+    public static void main(String args[]) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        bubbleSort(sortArr);
+        for(int i = 0; i < sortArr.length; i++){
+            System.out.print(sortArr[i] + "\n");
         }
+    }
+}
+
+
+    
+
+        
     
